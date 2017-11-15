4ed8e98a9a3d4eaa16b8e12c41006c8367311e32
yijigao <yijigao92@gmail.com>
2017-11-15 20:11:56 +0800

    "Overview The data"

--- OpenStreetMapProject/2.Codes/chengdu_database.py
+++ OpenStreetMapProject/2.Codes/chengdu_database.py
@@ -31,8 +31,19 @@ WHERE nodes_tags.key='cuisine'
 GROUP BY nodes_tags.value
 ORDER BY num DESC;
 """
+QUERY_Test = """
+SELECT nodes_tags.value, COUNT(*) as num
+FROM nodes_tags
+JOIN (SELECT DISTINCT(id) FROM nodes_tags WHERE value='restaurant')
+i
+ON nodes_tags.id=i.id
+WHERE nodes_tags.key='cuisine'
+GROUP BY nodes_tags.value
+ORDER BY num DESC
+LIMIT 10;
+"""
 
-c.execute(QUERY)
+c.execute(QUERY_Test)
 rows = c.fetchall()
 df = pd.DataFrame(rows)
 print(df)
