1105c2855fa22940605fdeeffb5638fbe87b0b12
yijigao <yijigao92@gmail.com>
2017-12-13 19:58:46 +0800

    explore the test with features

--- Machine Leaning/Choose your own algorithm/test_your_algotithm.py
+++ Machine Leaning/Choose your own algorithm/test_your_algotithm.py
@@ -1,6 +1,6 @@
 import sys
-from class_vis import prettyPicture, output_image
-from prep_terrain_data import makeTerrainData
+from Naive_bayes.class_vis import prettyPicture, output_image
+from Naive_bayes.prep_terrain_data import makeTerrainData
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,12 +9,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 # import my training data
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-names = ["Naive Bayes", "Linear SVM", "Decision Tree","Nearest Neighbor", "Random Forest", "AdaBoost"]
+names = ["Naive Bayes", "Linear SVM", "Decision Tree", "Nearest Neighbor", "Random Forest", "AdaBoost"]
 
 classifiers = [GaussianNB(),
                SVC(kernel="linear"),
@@ -23,11 +23,9 @@ classifiers = [GaussianNB(),
                RandomForestClassifier(n_estimators=2),
                AdaBoostClassifier(n_estimators=25)]
 
-
 for name, clf in zip(names, classifiers):
-	clf.fit(features_train, labels_train)
-	score = clf.score(features_test, labels_test)
-	print(f"{name}", score)
-	prettyPicture(name, clf, features_test, labels_test)
-	output_image(f'{name}.png', "png", open(f"{name}.png", "rb").read())
-
+    clf.fit(features_train, labels_train)
+    score = clf.score(features_test, labels_test)
+    print(f"{name}", score)
+    prettyPicture(name, clf, features_test, labels_test)
+    output_image(f'{name}.png', "png", open(f"{name}.png", "rb").read())
